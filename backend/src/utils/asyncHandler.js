6672ae88d67ad0e83abcc796617d4afb6182506e
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-07-05 18:27:18 +0530

    fix async handler

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
-const asyncHander = (fn = (req, res, next) => {
+const asyncHander = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
-});
+};
 
 export default asyncHander;
