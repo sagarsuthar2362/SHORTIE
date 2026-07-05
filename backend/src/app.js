a3fb5f94679ca028c3f7307a1e6e55542e23c8f3
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-07-06 00:12:52 +0530

    redirect controller and route work done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import morgan from "morgan";
+import urlRouter from "./routes/url.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+// url routes
+app.use("/api/url", urlRouter);
 
 export default app;
