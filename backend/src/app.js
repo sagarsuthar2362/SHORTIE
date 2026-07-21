ca9d496ab6f69f9420730062c6956175ca2ea721
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-07-21 23:58:25 +0530

    feat(backend): configure cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,24 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import morgan from "morgan";
 import urlRouter from "./routes/url.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+// configure cors
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    optionsSuccessStatus: 200,
+  }),
+);
 
 // url routes
 app.use("/api/url", urlRouter);
